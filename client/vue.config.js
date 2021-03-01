df9324ca4ee919a5ef83bbb043554a49ae75c442
waraidev <wileye92@gmail.com>
2021-02-28 20:43:20 -0500

    Added custom color for navbar and fixed python config

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,8 +1,20 @@
+const webpack = require('webpack')
+
 module.exports = {
-  lintOnSave: true,
-  configureWebpack: {
+    lintOnSave: true,
+    configureWebpack: {
         devServer: {
             historyApiFallback: true
-        }
-    }
+        },
+        plugins: [
+            new webpack.LoaderOptionsPlugin({
+                test: /\.scss$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            })
+        ]
+    },
 }
